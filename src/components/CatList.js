2cae72408d9aaaa3e06ab275813d3edf7c419b16
lambert19900603 <lambert19900603@163.com>
2016-08-03 09:37:08 +0800

    2016080301

--- src/components/CatList.js
+++ src/components/CatList.js
@@ -1,6 +1,5 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Glyphicon } from 'react-bootstrap';
 
 class CatList extends Component {
     static propTypes = {
