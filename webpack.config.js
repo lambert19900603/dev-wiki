c01bf48c930d8a5faa4eea9198c912e5a32bf10e
lambert19900603 <lambert19900603@163.com>
2016-08-03 09:43:56 +0800

    2016080302

--- webpack.config.js
+++ webpack.config.js
@@ -43,8 +43,8 @@ module.exports = {
             test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
             loader: 'file'
         }, {
-            test: /\.(woff|woff2)/,
-            loader: 'url?limit=10000'
+            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'url?limit=10000&mimetype=application/font-woff'
         }, {
             test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
             loader: 'url?limit=10000&mimetype=application/octet-stream'
